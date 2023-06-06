12e9d96cf146412b498f63fc7b3e90e32fdfb6cc
Steven E. Harris <seh@panix.com>
2023-06-06 12:28:50 -0400

    Use database transactions in HTTP handlers

--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
+
+	idb "sehlabs.com/db/internal/db"
 )
 
 func speakPlainTextTo(w http.ResponseWriter) {
@@ -14,96 +18,186 @@ func speakPlainTextTo(w http.ResponseWriter) {
 // 	w.Header().Add("Content-Type", "application/json")
 // }
 
+func respondWithError(w http.ResponseWriter, err error) {
+	statusCode := http.StatusInternalServerError
+	if errors.Is(err, idb.ErrTransactionInConflict) {
+		statusCode = http.StatusConflict
+	}
+	speakPlainTextTo(w)
+	w.WriteHeader(statusCode)
+	fmt.Fprintln(w, err)
+}
+
+const pathPrefix = "/record/"
+
+func getTargetKey(w http.ResponseWriter, req *http.Request) (idb.Key, bool) {
+	key, ok := strings.CutPrefix(req.URL.Path, pathPrefix)
+	if ok && len(key) > 0 {
+		return idb.Key(key), true
+	}
+	speakPlainTextTo(w)
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintln(w, "URL path must contain a nonempty key")
+	return nil, false
+}
+
+func handleGet(ctx context.Context, w http.ResponseWriter, req *http.Request, db database) {
+	key, ok := getTargetKey(w, req)
+	if !ok {
+		return
+	}
+	var recordExists bool
+	var value idb.Value
+	if err := db.WithinTransaction(ctx, func(ctx context.Context, tx idb.Transaction) (bool, error) {
+		v, err := tx.Get(ctx, key)
+		if errors.Is(err, idb.ErrRecordDoesNotExist) {
+			return false, nil
+		}
+		if err != nil {
+			return false, err
+		}
+		recordExists = true
+		v.CopyInto(&value)
+		return false, nil
+	}); err != nil {
+		respondWithError(w, err)
+		return
+	}
+	if !recordExists {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		speakPlainTextTo(w)
+		if _, err := w.Write(value); err == nil {
+			w.Write([]byte{'\n'})
+		}
+	}
+}
+
+func handlePost(ctx context.Context, w http.ResponseWriter, req *http.Request, db database) {
+	if err := req.ParseForm(); err != nil {
+		speakPlainTextTo(w)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Failed to parse HTTP form: %v", err)
+		return
+	}
+	key, ok := getTargetKey(w, req)
+	if !ok {
+		return
+	}
+	value := req.FormValue("value")
+	var recordExisted bool
+	if err := db.WithinTransaction(ctx, func(ctx context.Context, tx idb.Transaction) (bool, error) {
+		err := tx.Insert(ctx, key, idb.Value(value))
+		if errors.Is(err, idb.ErrRecordExists) {
+			recordExisted = true
+			return false, nil
+		}
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	}); err != nil {
+		respondWithError(w, err)
+	}
+	if recordExisted {
+		w.WriteHeader(http.StatusConflict)
+	} else {
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func handlePut(ctx context.Context, w http.ResponseWriter, req *http.Request, db database) {
+	key, ok := getTargetKey(w, req)
+	if !ok {
+		return
+	}
+	value := req.FormValue("value")
+	type updatePolicy uint
+	const (
+		abortIfAbsent updatePolicy = iota
+		insertIfAbsent
+		ignoreIfAbsent
+	)
+	policy := abortIfAbsent
+	{
+		const formKey = "if-absent"
+		ifAbsent := req.FormValue(formKey)
+		switch ifAbsent {
+		case "", "abort":
+		case "insert":
+			policy = insertIfAbsent
+		case "ignore":
+			policy = ignoreIfAbsent
+		default:
+			speakPlainTextTo(w)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Unrecognized HTTP form key %q value: %q\n", formKey, ifAbsent)
+			return
+		}
+	}
+	if policy == insertIfAbsent {
+		// TODO(seh): Implement Upsert.
+		w.WriteHeader(http.StatusNotImplemented)
+	} else {
+		var recordExisted bool
+		if err := db.WithinTransaction(ctx, func(ctx context.Context, tx idb.Transaction) (bool, error) {
+			err := tx.Update(ctx, key, idb.Value(value))
+			if errors.Is(err, idb.ErrRecordDoesNotExist) {
+				return false, nil
+			}
+			if err != nil {
+				return false, err
+			}
+			recordExisted = true
+			return true, nil
+		}); err != nil {
+			respondWithError(w, err)
+		}
+		if !recordExisted && policy == abortIfAbsent {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func handleDelete(ctx context.Context, w http.ResponseWriter, req *http.Request, db database) {
+	key, ok := getTargetKey(w, req)
+	if !ok {
+		return
+	}
+	var recordExisted bool
+	if err := db.WithinTransaction(ctx, func(ctx context.Context, tx idb.Transaction) (bool, error) {
+		err, deleted := tx.Delete(ctx, key)
+		if err != nil {
+			return false, err
+		}
+		recordExisted = deleted
+		return true, nil
+	}); err != nil {
+		respondWithError(w, err)
+		return
+	}
+	if !recordExisted {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
 func makeHandler(db database) http.Handler {
 	var mux http.ServeMux
 	{
-		const pathPrefix = "/record/"
 		mux.Handle(pathPrefix,
 			http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				getTargetKey := func() (string, bool) {
-					key, ok := strings.CutPrefix(req.URL.Path, pathPrefix)
-					if ok && len(key) > 0 {
-						return key, true
-					}
-					w.WriteHeader(http.StatusBadRequest)
-					// TODO(seh): Write this as JSON.
-					speakPlainTextTo(w)
-					fmt.Fprintln(w, "URL path must contain a nonempty key")
-					return "", false
-				}
 				switch req.Method {
 				case http.MethodGet:
-					key, ok := getTargetKey()
-					if !ok {
-						return
-					}
-					// TODO(seh): Implement Get.
-					fmt.Fprintf(w, "Key: %q\n", key)
-					return
+					handleGet(req.Context(), w, req, db)
 				case http.MethodPost:
-					if err := req.ParseForm(); err != nil {
-						w.WriteHeader(http.StatusBadRequest)
-						// TODO(seh): Write this as JSON.
-						speakPlainTextTo(w)
-						fmt.Fprintf(w, "Failed to parse HTTP form: %v", err)
-						return
-					}
-					key, ok := getTargetKey()
-					if !ok {
-						return
-					}
-					value := req.FormValue("value")
-					// TODO(seh): Implement Insert.
-					// TODO(seh): Return status code 201 if we inserted a record.
-					fmt.Fprintf(w, "Key: %q -> Value: %q\n", key, value)
-					return
+					handlePost(req.Context(), w, req, db)
 				case http.MethodPut:
-					key, ok := getTargetKey()
-					if !ok {
-						return
-					}
-					value := req.FormValue("value")
-					type updatePolicy uint
-					const (
-						abortIfAbsent updatePolicy = iota
-						insertIfAbsent
-						ignoreIfAbsent
-					)
-					policy := abortIfAbsent
-					{
-						const formKey = "if-absent"
-						ifAbsent := req.FormValue(formKey)
-						switch ifAbsent {
-						case "", "abort":
-						case "insert":
-							policy = insertIfAbsent
-						case "ignore":
-							policy = ignoreIfAbsent
-						default:
-							w.WriteHeader(http.StatusBadRequest)
-							// TODO(seh): Write this as JSON.
-							speakPlainTextTo(w)
-							fmt.Fprintf(w, "Unrecognized HTTP form key %q value: %q\n", formKey, ifAbsent)
-							return
-						}
-					}
-					fmt.Fprintf(w, "Key: %q -> Value: %q\n", key, value)
-					if policy == insertIfAbsent {
-						// TODO(seh): Implement Upsert.
-					} else {
-						// TODO(seh): Implement Update, taking the policy into account when handling
-						// the db.ErrRecordDoesNotExist error.
-					}
+					handlePut(req.Context(), w, req, db)
 				case http.MethodDelete:
-					/*key*/ _, ok := getTargetKey()
-					if !ok {
-						return
-					}
-					// TODO(seh): Implement Delete.
+					handleDelete(req.Context(), w, req, db)
 				default:
-					w.WriteHeader(http.StatusBadRequest)
-					// TODO(seh): Write this as JSON.
 					speakPlainTextTo(w)
+					w.WriteHeader(http.StatusBadRequest)
 					fmt.Fprintf(w, "Request uses disallowed HTTP method %q\n", req.Method)
 					return
 				}
